993ca700875630cea78299e7fd2e1d130ae3532b
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2018-03-18 14:21:48 +0530

    Change card token creation order

--- subscription.js
+++ subscription.js
@@ -22,11 +22,12 @@ const stripe = require('stripe')('<api-key>');
  * Create a customer
  * @param {} email email address of the customer
  */
-function createCustomer(email) {
+function createCustomer(email, token) {
   return new Promise(function (resolve, reject) {
     console.log('Creating customer...');
     stripe.customers.create({
-      email: email
+      email: email,
+      source: token
     }).then(function (customer) {
       console.log("Customer created: [id] " + customer.id);
       resolve(customer);
@@ -39,16 +40,15 @@ function createCustomer(email) {
 
 /**
  * Create a card token by providing credit/debit card information
- * @param {*} customer Customer entity
  * @param {*} token Card token
  */
-function createCardToken(customer, token) {
+function createCardToken(token) {
   return new Promise(function (resolve, reject) {
     console.log("Creating card token...");
     stripe.tokens.create(token).then(
       function (token) {
         console.log("Card token created: [token-id] " + token.id);
-        return resolve(customer, token);
+        return resolve(token);
       }).catch(function (err) {
         console.log("Could not create card token: " + err.message);
         return reject(err);
@@ -80,36 +80,55 @@ function subscribeCustomerToPlan(customer, planId) {
   });
 }
 
+/**
+ * List invoices of customer
+ * @param {*} customerId Customer id
+ */
+function listInvoices(customerId) {
+  return new Promise(function (resolve, reject) {
+    console.log("Listing invoices of customer: [customer-id] " + customerId);
+    stripe.invoices.list({ customer: customerId })
+      .then(function (invoices) {
+        resolve(invoices);
+      })
+      .catch(function (err) {
+        log.console("Could not list invoices: " + err.message);
+        reject(err);
+      });
+  });
+}
+
 var planId = "<plan-id>";
 // A test credit card information
 var token = {
   card: {
     "number": '4242424242424242',
     "exp_month": 12,
-    "exp_year": 2019,
+    "exp_year": 2020,
     "cvc": '123'
   }
 };
 
-// Create customer
-createCustomer("foo2@example.com", "tok_visa").then(
-  function (customer) {
-    // Create card token
-    createCardToken(customer, token)
+// Create card token
+createCardToken(token).then(
+  function (token) {
+    // Create customer with email and card token
+    createCustomer("foo@example.com", token.id)
       .then(function (customer, token) {
-        // Add card token to customer
-        addCardTokenToCustomer(customer, token)
-          .then(function (customer) {
-            // Subscribe customer to plan
-            subscribeCustomerToPlan(customer, planId)
-              .catch(function (err) {
-                console.log(err);
-              });
+        // Subscribe customer to plan
+        subscribeCustomerToPlan(customer, planId)
+          .then(function (response) {
+            // List customer invoices
+            listInvoices(customer.id)
+              .then(function (invoices) {
+                console.log(invoices);
+              })
           })
           .catch(function (err) {
             console.log(err);
           });
-      }).catch(function (err) {
+      })
+      .catch(function (err) {
         console.log(err);
       });
   }).catch(function (err) {
